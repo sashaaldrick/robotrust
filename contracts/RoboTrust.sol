02feb03757177e3627baa1402f3543f3563813a0
Alexander Aldrick <alex@aldrick.dev>
2022-02-13 16:58:22 -0700

    finished gelato implentation for rinkeby with resolver

--- contracts/RoboTrust.sol
+++ contracts/RoboTrust.sol
@@ -2,17 +2,24 @@
 pragma solidity 0.8.11;
 
 import "hardhat/console.sol";
-import "@"
+
 contract RoboTrust {
 
     address payable public owner;
+    uint public lastExecuted;
 
     constructor() payable {
         owner = payable(msg.sender); // convert msg.sender to payable.
     }
 
-    function payOwner(uint _amount) public {
+    function payOwner(uint256 _amount) external {
         (bool sent, ) = owner.call{value: _amount}("");
         require(sent, "Failed to send Ether");
+        lastExecuted = block.number;
     }
+
+    function getLastExecuted() external view returns(uint) {
+        return lastExecuted;
+    }
+
 }
--- scripts/deploy_resolver.js
+++ scripts/deploy_resolver.js
@@ -8,24 +8,24 @@ async function main() {
     const apiKey = process.env.INFURA_API_KEY;
     const privKey = process.env.DEVNET_PRIVKEY;
     provider = new ethers.providers.InfuraProvider("rinkeby", apiKey);
+    const RoboTrustAddress = "0xEa9a87800886CBD3244DFA617E1B644E16b01309";
   
     // create layer 1 wallet.
     let wallet = new ethers.Wallet(privKey, provider);
     console.log('Your wallet address:', wallet.address);
   
     // connect signer to contract object.
-    const RoboTrustContract = await (
-      await hre.ethers.getContractFactory("RoboTrust")
+    const ResolverContract = await (
+      await hre.ethers.getContractFactory("Resolver")
     ).connect(wallet);
-    console.log('Deploying RoboTrust contract to Rinkeby');
+    console.log('Deploying Resolver contract to Rinkeby');
 
     // deploy contract.
-    const initialBalance = ethers.utils.parseEther("0.1");
-    const contract = await RoboTrustContract.deploy({ value: initialBalance});
+    const contract = await ResolverContract.deploy(RoboTrustAddress);
     await contract.deployed();
     
     // success!
-    console.log(`RoboTrust contract is deployed to ${contract.address}`)
+    console.log(`Resolver contract is deployed to ${contract.address}`)
   }
   
   main()
