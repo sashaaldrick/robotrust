ef2f874dbbe7f111dfcef3aa43cdc3d8cf18fb11
AntwuanDixon <evanwchipman@gmail.com>
2022-02-19 13:03:38 -0700

    refactor: hardcode each input field, add styling

--- src/components/form/AmountForm.js
+++ src/components/form/AmountForm.js
@@ -1,25 +1,9 @@
-import { Box, Typography, FormLabel, Switch as MaterialSwitch, FormControlLabel } from '@mui/material'
-import { useField } from 'formik'
+import { Box, Typography, Divider, Switch } from '@mui/material'
 import React, { useState } from 'react'
 
 import { InputField as Field } from './fields/input'
 import { SelectField as Select } from './fields/select'
-import useGetETHtoUSD from '../../hooks/useGetETHtoUSD'
-
-function ETHInput(props) {
-    const [field, meta] = useField(props);
-    const USD = useGetETHtoUSD(field.value ? field.value : 0)
-    return (
-      <>
-        <Field {...field} {...props} />
-        {meta.error && meta.touched && <div>{meta.error}</div>}
-        <FormLabel label="Amount in USD"/>
-        <Typography variant="h5" component="h5" style={{color: 'white'}}>
-            ${USD}
-        </Typography>
-      </>
-    );
-  }
+import { ETHInput } from './fields/ETHInput'
 
 export const AmountForm = ({ formField, formProps }) => {
     //   const [field, meta] = useField(formProps)
@@ -27,7 +11,6 @@ export const AmountForm = ({ formField, formProps }) => {
   const {termInYears, ethAmount, trusteeAddress, beneficiaryAddress, retainedInterest, graduatedToggle, beneficiaryToggle, interestRate, firstPayment } = formField
   const updateBeneficiaryToggle = () => {
       setBeneficiaryVis(!beneficiaryVis)
-
   }
   return (
     <>
@@ -39,13 +22,14 @@ export const AmountForm = ({ formField, formProps }) => {
             borderRadius: '1rem',
             display: 'flex',
           }}
+          className="client"
         >
             <Box
               sx={{
                 '& > :not(style)': {
                   m: 3,
                   pb: 5.0,
-                  width: '18rem',
+                //   width: '18rem',
                   padding: '.2rem',
                 },
               }}
@@ -56,36 +40,78 @@ export const AmountForm = ({ formField, formProps }) => {
                     label={ethAmount.label}
                     placeholder={ethAmount.placeholder}
                 />
-                <Field 
-                    name={termInYears.name}
-                    label={termInYears.label}
-                    placeholder={termInYears.placeholder}
-                />
-                <Field 
-                    name={trusteeAddress.name}
-                    label={trusteeAddress.label}
-                    placeholder={trusteeAddress.placeholder}
-                />
-                <Field 
-                    name={beneficiaryAddress.name}
-                    label={beneficiaryAddress.label}
-                    placeholder={beneficiaryAddress.placeholder}
-                />
-                <Field 
-                    name={retainedInterest.name}
-                    label={retainedInterest.label}
-                    placeholder={retainedInterest.placeholder}
-                />
+                <Typography variant="h3" className="gradient__text">Enter GRAT provisions:</Typography>
+                <Divider />
+                <Box sx={{
+                        display: 'flex',
+                        justifyContent: 'space-between'
+                    }}>
+                    <Box sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        width: '40%',
+                    }}>
+                        <Field 
+                            name={trusteeAddress.name}
+                            label={trusteeAddress.label}
+                            placeholder={trusteeAddress.placeholder}
+                        />
+                        <Typography style={{color: '#FF8A71'}}>The Trustee has the ability to liquidate the Trust, 
+                            in which case all assets of the Trust will be transferred to 
+                            the then current Trustee Wallet. The Trustee also has the ability 
+                            to assign Trustee control to another wallet.
+                        </Typography>
+                    </Box>
+                    <Box sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        width: '40%'
+                    }}>
+                        <Field 
+                            name={beneficiaryAddress.name}
+                            label={beneficiaryAddress.label}
+                            placeholder={beneficiaryAddress.placeholder}
+                        />
+                    </Box>
+                </Box>
+                <Box sx={{
+                        display: 'flex',
+                        justifyContent: 'space-between'
+                    }}>
+                    <Box sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        width: '40%',
+                    }}>
+                        <Field 
+                            name={retainedInterest.name}
+                            label={retainedInterest.label}
+                            placeholder={retainedInterest.placeholder}
+                        />
+                        <Typography style={{color: '#FF8A71'}}>The estimated gift includable in your estate is equal to the grant amount minus the retained interest.</Typography>
+                    </Box>
+                    <Box sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        width: '40%'
+                    }}>
+                        <Field 
+                            name={termInYears.name}
+                            label={termInYears.label}
+                            placeholder={termInYears.placeholder}
+                        />
+                        <Typography style={{color: '#FF8A71'}}>Warning: Setting this value lower than ‘2’ may result in nonrecognition by the IRS.</Typography>
+                    </Box>
+                </Box>
+                <Typography>Interest Rate: 1.69%</Typography>
+                <Switch {...label} />
                 <div>
                 <label className='switch'>
+                    <Typography className="gradient__text"> Allow Beneficiary to view accounting? </Typography>
                     <Field type="checkbox" name={beneficiaryToggle.name} label={beneficiaryToggle.label} />
-                    <span className="slider round"></span>
+                    {/* <span className="slider round"></span> */}
                     {`${beneficiaryToggle.toggle}`}
                 </label>
-                {/* <label className="switch">
-                    <input type="checkbox" onChange={e => setBeneficiaryToggle(!beneficiaryToggle)}/>
-                    <span className="slider round"></span>
-                </label> */}
                 </div>
                 <Field 
                     name={graduatedToggle.name}
@@ -97,23 +123,12 @@ export const AmountForm = ({ formField, formProps }) => {
                     label={graduatedToggle.label}
                     placeholder={graduatedToggle.placeholder}
                 />
-
-                  {/* {Object.values(formField).map((field, index) => (
-                    <Field
-                        key={index}
-                        size={''}
-                        name={field.name}
-                        placeholder={field.label}
-                        label={field.label}
-                        // meta={meta}
-                    />
-                  ))} */}
-                  {/* <Select 
+                  <Select 
                     size={''}
                     name={termInYears.name}
                     placeholder={termInYears.label}
                     label={termInYears.label}
-                  /> */}
+                  />
             </Box>
           </Box>
     </>
